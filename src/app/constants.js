ff8470662b8767ec9f85f1194186912ac8cb2dfb
Elliott <ejbabb@gmail.com>
2025-11-03 14:27:28 -0800

    Cleanup homepage and main styling

---
+++ src/app/constants.js
@@ -0,0 +1,2 @@
+export const HEADER_TEXT = 'Solace Healthcare';
+export const BUTTON_TEXT = 'Find your Advocate now!';
---
+++ src/app/styles.js
@@ -0,0 +1,25 @@
+export const styles = {
+  container: {
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'column',
+    gap: '200px',
+    margin: '80px',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: '48px',
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  buttonContainer: {
+    fontSize: '16px',
+    backgroundColor: 'gold',
+    padding: '40px',
+    borderRadius: 8,
+  },
+  buttonText: {
+    fontSize: '16px',
+    color: 'black',
+  },
+};
